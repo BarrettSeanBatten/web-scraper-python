3d9b291bb4c9229dd7ac63757fb2d35afa89b519
spumel534 <spumel534@gmail.com>
2023-07-07 09:24:51 -0400

    utilized old.reddit website

--- materials-web-scraping-tutorial/scrape_selenium.py
+++ materials-web-scraping-tutorial/scrape_selenium.py
@@ -3,20 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+link = 'https://old.reddit.com/r/aliens/comments/14rp7w9/from_the_late_2000s_to_the_mid2010s_i_worked_as_a/'
+driver.get(link)
 
-driver.get('https://www.reddit.com/r/aliens/comments/14rp7w9/from_the_late_2000s_to_the_mid2010s_i_worked_as_a/');
-
-time.sleep(2)
-
-#driver.find_element(By.CLASS_NAME, "m-0").click()
-driver.execute_script("window.scrollTo(0, 9999999999999999999999999999999999999999999999999);")
-
-time.sleep(1)
-
+time.sleep(0.5)
+driver.execute_script("window.scrollTo(0, 999999999999999999999999999999999999);")
+time.sleep(0.5)
 elements = driver.find_elements(By.TAG_NAME,"p")
 
 for element in elements:
-    if not element.text == '':
+    if not (element.text == '' or element.text.__contains__('[–]')):
         print(element.text)
 
 driver.quit()
